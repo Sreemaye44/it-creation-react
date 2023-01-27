0d29031260a61762a9c591a08efd19750909a099
sreemaye <sreemaye44@gmail.com>
2023-01-27 12:31:48 +0600

    login page created

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {FaEye, FaUserCircle } from 'react-icons/fa';
+
+const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+    }
+    return (
+        <div className='w-10/12 mx-auto'>
+            <h1 className='font-bold text-3xl m-10'>IT <span className='text-violet-500'>CReation</span></h1>
+            <div>
+                <h2 className='font-bold text-xl'>HELLO SIGN IN</h2>
+                <form onSubmit={handleLogin}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">User ID</span>
+          </label>
+                        <div className='relative flex'>
+                        <input type="email" name="email" placeholder="Emp User Id" className="input input-bordered w-full" required />
+                            <div className='absolute top-1/2 transform -translate-y-1/2 right-3 text-slate-600'><FaUserCircle></FaUserCircle></div>
+                       </div>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <div className='relative flex'>
+                        <input type="email" name="password" placeholder="********" className="input input-bordered w-full" required />
+                            <div className='absolute top-1/2 transform -translate-y-1/2 right-3 text-slate-600'><FaEye></FaEye></div>
+                       </div>
+          <div class="my-6">
+                            <div class="flex items-center justify-between">
+                                <div>
+                                    <label class="block" for="remember">
+                                        <input class="ml-2 leading-tight" type="checkbox" id="remember" name="remember"/>
+                                        <span class="text-sm mx-2 ">
+                                            Remember me
+                                        </span>
+                                    </label>
+                                </div>
+                                <div>
+                                    <a class="text-sm text-violet-500" href="#password-request">
+                                        forgot password
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+        </div>
+        <div className="form-control mt-6">
+                        <input className='btn bg-violet-500 border-none mb-5' type="submit" value="Sign In" />
+                        <div className='mt-10 text-sm'>
+                            Don't have an account? <span>Sign up</span>
+                        </div>
+        
+        </div>
+        </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
